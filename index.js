d9179cf3a63ed30e0fc0e193ebc806f7f76c8f13
Carolina Pinzon <carolina.pinzon@lab21k.com.br>
2016-04-20 16:02:39 -0500

    support for multiple pairs, adding by external button

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-var radio = 50;
+var radio = 100;
 var degreesStart = 0;
 var degreesEnd = 0;
 
 var arrayMarkerPairs = [];
 
 function rotateAnnotationCropper(offsetSelector, xCoordinate, yCoordinate, cropper, startOrEnd, arc, index){
-    //alert(offsetSelector.left);
-
-    //console.log(offsetSelector.width());
-
     var x = xCoordinate - offsetSelector.offset().left - offsetSelector.width()/2;
     var y = -1*(yCoordinate - offsetSelector.offset().top - offsetSelector.height()/2);
     var theta = Math.atan2(y,x)*(180/Math.PI);        
 
-
     var cssDegs = convertThetaToCssDegs(theta);
     var rotate = 'rotate(' +cssDegs + 'deg)';
     cropper.css({'-moz-transform': rotate, 'transform' : rotate, '-webkit-transform': rotate, '-ms-transform': rotate});
-    //console.log(cssDegs);
     var radioP = radio - 2.5;
     if (startOrEnd === 'start') {
         arrayMarkerPairs[index].startAngle = cssDegs;
@@ -25,9 +19,8 @@ function rotateAnnotationCropper(offsetSelector, xCoordinate, yCoordinate, cropp
     else {
         arrayMarkerPairs[index].endAngle = cssDegs;
     }
-    arc.children()[0].setAttribute("d", describeArc(radio, radio, radioP, arrayMarkerPairs[index].startAngle, arrayMarkerPairs[index].endAngle));  
+    arc.childNodes[0].setAttributeNS(null, "d", describeArc(radio, radio, radioP, arrayMarkerPairs[index].startAngle, arrayMarkerPairs[index].endAngle));  
     $('body').on('mouseup', function(event){ $('body').unbind('mousemove')});
-
 }
     
 function convertThetaToCssDegs(theta){
@@ -37,27 +30,6 @@ function convertThetaToCssDegs(theta){
     }
     return cssDegs;
 }
-
-$(document).ready(function(){  
-    var firstPair =  {
-        markerStart: $('#marker_0_start'),
-        markerEnd : $('#marker_0_end'),
-        arc: $('#arc_0'),
-        startAngle: 0,
-        endAngle: 0
-    };
-    arrayMarkerPairs.push(firstPair);
-
-    var diametro = radio*2;
-
-    $('#dynamic-container')[0].style.width = diametro + 'px'; 
-    $('#dynamic-container')[0].style.height = diametro + 'px';
-    $('#innerCircle')[0].style.width = diametro + 'px';
-    $('#innerCircle')[0].style.height = diametro + 'px';
-    $('#innerCircle')[0].style.marginBottom = -diametro + 'px';
-
-    initLastPair();                
-}); 
 
 function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
     var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
@@ -79,7 +51,6 @@ function describeArc(x, y, radius, startAngle, endAngle){
     }
 
     var arcSweep = endAngle - startAngle <= 180 ? "0" : "1";
-    //console.log(endAngle - startAngle);
 
     var d = [
         "M", start.x, start.y, 
@@ -107,31 +78,43 @@ function initLastPair() {
 
     initMarker(currentMarkerPair.markerStart, currentMarkerPair.arc, index);
     initMarker(currentMarkerPair.markerEnd, currentMarkerPair.arc, index);
-
-    var diametro = radio*2;
-    currentMarkerPair.arc[0].style.width = diametro + 'px'; 
-    currentMarkerPair.arc[0].style.height = diametro + 'px';
-    currentMarkerPair.arc[0].style.top = -diametro + 'px';
 }
 
 function createPair() {
+    var diametro = radio*2;
     var lastPair = {
         startAngle: 0,
         endAngle: 0
     };
     lastPair.markerStart = $('<div>', { id: "marker_" + arrayMarkerPairs.length + "_start", class: "marker"});
     lastPair.markerEnd = $('<div>', { id: "marker_" + arrayMarkerPairs.length + "_end", class: "marker"});
-    lastPair.arc = $('<svg>', { id: "arc_" + arrayMarkerPairs.length, class: "arc"});
-    lastPair.arc.append(
-        $('<path>', { fill: "none", stroke: "#446688", "stroke-width":"5"})
-        );
+    lastPair.arc = document.createElementNS("http://www.w3.org/2000/svg","svg");
+    lastPair.arc.setAttributeNS(null, "class", "arc"); 
+    lastPair.arc.style.width = diametro + 'px';
+    lastPair.arc.style.height = diametro + 'px';
+    lastPair.arc.style.top = -diametro + 'px';
+    lastPair.arc.style.marginRight = -diametro + 'px';
+    var newpath = document.createElementNS("http://www.w3.org/2000/svg","path");  
+        newpath.setAttributeNS(null, "stroke", "#446688"); 
+        newpath.setAttributeNS(null, "stroke-width", 5); 
+        newpath.setAttributeNS(null, "fill", "none");
+    lastPair.arc.appendChild(newpath);
+
     $('#dynamic-container').append([lastPair.markerStart[0], lastPair.markerEnd[0]]);
     $('#time-pair-container').append(lastPair.arc);
     arrayMarkerPairs.push(lastPair);
-    //console.log
     initLastPair();
-}
+} 
 
-window.setTimeout(function() {
-    createPair()
-}, 1000);  
+$(document).ready(function(){  
+    var diametro = radio*2;
+
+    $('#dynamic-container')[0].style.width = diametro + 'px'; 
+    $('#dynamic-container')[0].style.height = diametro + 'px';
+    $('#innerCircle')[0].style.width = diametro + 'px';
+    $('#innerCircle')[0].style.height = diametro + 'px';
+    $('#innerCircle')[0].style.marginBottom = -diametro + 'px';
+
+    createPair();  
+        
+}); 
